6a4541a357b3b8f4965efad94728d9a5711fb1c6
Cindy Xiao <git@cxiao.net>
2022-12-24 11:29:30 -0800

    refactor: Improve logging in api module

--- hashdb_api.py
+++ hashdb_api.py
@@ -1,5 +1,8 @@
 from typing import List
 import requests
+import binaryninja
+
+logger = binaryninja.log.Logger(session_id=0, logger_name=__name__)
 
 
 class HashDBError(Exception):
@@ -8,6 +11,7 @@ class HashDBError(Exception):
 
 def get_algorithms(api_url: str) -> List[str]:
     request_url = api_url + "/hash"
+    logger.log_info(f"get_algorithms requested URL: {request_url}")
     r = requests.get(request_url)
     if not r.ok:
         raise HashDBError(
@@ -20,6 +24,7 @@ def get_algorithms(api_url: str) -> List[str]:
 
 def get_strings_from_hash(algorithm: str, hash_value: int, api_url: str):
     request_url = api_url + "/hash/%s/%d" % (algorithm, hash_value)
+    logger.log_info(f"get_strings_from_hash requested URL: {request_url}")
     r = requests.get(request_url)
     if not r.ok:
         raise HashDBError(
@@ -31,6 +36,7 @@ def get_strings_from_hash(algorithm: str, hash_value: int, api_url: str):
 
 def get_module_hashes(module_name: str, algorithm: str, permutation: str, api_url: str):
     request_url = api_url + "/module/%s/%s/%s" % (module_name, algorithm, permutation)
+    logger.log_info(f"get_module_hashes requested URL: {request_url}")
     r = requests.get(request_url)
     if not r.ok:
         raise HashDBError(
@@ -44,11 +50,14 @@ def hunt_hash(hash_value: int, api_url: str) -> List:
     matches = []
     hash_list = [hash_value]
     request_url = api_url + "/hunt"
+    logger.log_info(f"hunt_hash requested URL: {request_url}")
     r = requests.post(request_url, json={"hashes": hash_list})
     if not r.ok:
-        log_info(request_url)
-        log_info(hash_list)
-        log_info(r.json())
+        logger.log_error(
+            f"hunt_hash request to URL {request_url} failed, using the following parameters:"
+        )
+        logger.log_error(f"hash list: {hash_list}")
+        logger.log_error(f"request body: {r.json()}")
         raise HashDBError(
             f"Get hash API request failed, status {r.status_code} for URL: {request_url}"
         )
