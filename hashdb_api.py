4c28b66402f599372ec5260836493246145833dd
Cindy Xiao <git@cxiao.net>
2022-12-27 03:14:34 -0800

    refactor: Use dedicated classes for return types from hashdb_api

--- hashdb_api.py
+++ hashdb_api.py
@@ -1,6 +1,8 @@
-from typing import List
-import requests
+from dataclasses import dataclass
+from typing import Any, Dict, List, Optional, Self, Type
+from urllib.parse import urljoin
 import binaryninja
+import requests
 
 logger = binaryninja.log.Logger(session_id=0, logger_name=__name__)
 
@@ -9,60 +11,179 @@ class HashDBError(Exception):
     pass
 
 
-def get_algorithms(api_url: str) -> List[str]:
-    request_url = api_url + "/hash"
-    logger.log_info(f"get_algorithms requested URL: {request_url}")
+@dataclass
+class Algorithm:
+    algorithm: str
+    description: str
+    type: str
+
+    @classmethod
+    def from_dict(cls: Type[Self], src: Dict[str, Any]) -> Self:
+        result = cls(
+            algorithm=src["algorithm"],
+            description=src["description"],
+            type=src["type"],
+        )
+        return result
+
+    def __str__(self) -> str:
+        return f"{self.algorithm} ({self.type}): {self.description}"
+
+
+@dataclass
+class HashString:
+    string: str
+    is_api: bool
+    permutation: Optional[str]
+    api: Optional[str]
+    modules: Optional[List[str]]
+
+    @classmethod
+    def from_dict(cls: Type[Self], src: Dict[str, Any]) -> Self:
+        result = cls(
+            string=src["string"],
+            is_api=src["is_api"],
+            permutation=src.get("permutation"),
+            api=src.get("api"),
+            modules=src.get("modules"),
+        )
+        return result
+
+    def get_api_string_if_available(self) -> str:
+        if self.is_api and self.api is not None:
+            return self.api
+        else:
+            return self.string
+
+
+@dataclass
+class Hash:
+    value: int
+    hash_string: HashString
+
+    @classmethod
+    def from_dict(cls: Type[Self], src: Dict[str, Any]) -> Self:
+        result = cls(
+            value=src["hash"],
+            hash_string=HashString.from_dict(src["string"]),
+        )
+        return result
+
+
+@dataclass
+class HuntMatch:
+    algorithm: str
+    count: int
+    hitrate: int
+
+    @classmethod
+    def from_dict(cls: Type[Self], src: Dict[str, Any]) -> Self:
+        result = cls(
+            algorithm=src["algorithm"],
+            count=src["count"],
+            hitrate=src["hitrate"],
+        )
+        return result
+
+
+def get_algorithms(api_url: str) -> List[Algorithm]:
+    request_url = urljoin(api_url, "/hash")
+    logger.log_debug(f"get_algorithms requested URL: {request_url}")
+
     r = requests.get(request_url)
     if not r.ok:
         raise HashDBError(
-            f"Get hash API request failed, status {r.status_code} for URL: {request_url}"
+            f"Get algorithm API request failed for URL {request_url} with status code {r.status_code}"
         )
+
     results = r.json()
-    algorithms = [a.get("algorithm") for a in results.get("algorithms", [])]
-    return algorithms
+    logger.log_debug(
+        f"get_algorithms request to URL: {request_url} returned results\n{results}"
+    )
+    try:
+        algorithms = [
+            Algorithm.from_dict(algorithm) for algorithm in results["algorithms"]
+        ]
+    except KeyError as parsing_key_error:
+        raise HashDBError(
+            f"Could not parse the following response from URL {request_url} as a valid list of algorithms; parsing failed to find required key {parsing_key_error}:\n{results}"
+        )
+    return sorted(algorithms, key=lambda algorithm: algorithm.algorithm)
 
 
-def get_strings_from_hash(algorithm: str, hash_value: int, api_url: str):
-    request_url = api_url + "/hash/%s/%d" % (algorithm, hash_value)
-    logger.log_info(f"get_strings_from_hash requested URL: {request_url}")
+def get_strings_from_hash(algorithm: str, hash_value: int, api_url: str) -> List[Hash]:
+    request_url = urljoin(api_url, f"/hash/{algorithm:s}/{hash_value:d}")
+    logger.log_debug(f"get_strings_from_hash requested URL: {request_url}")
+
     r = requests.get(request_url)
     if not r.ok:
         raise HashDBError(
-            f"Get hash API request failed, status {r.status_code} for URL: {request_url}"
+            f"Get hash API request failed for URL {request_url} with status code {r.status_code}"
         )
+
     results = r.json()
-    return results
+    logger.log_debug(
+        f"get_strings_from_hash request to URL: {request_url} returned results\n{results}"
+    )
+    try:
+        hashes = [Hash.from_dict(hash_) for hash_ in results["hashes"]]
+    except KeyError as parsing_key_error:
+        raise HashDBError(
+            f"Could not parse the following response from URL {request_url} as a valid list of hashes; parsing failed to find required key {parsing_key_error}:\n{results}"
+        )
+    return hashes
 
 
-def get_module_hashes(module_name: str, algorithm: str, permutation: str, api_url: str):
-    request_url = api_url + "/module/%s/%s/%s" % (module_name, algorithm, permutation)
-    logger.log_info(f"get_module_hashes requested URL: {request_url}")
+def get_module_hashes(
+    module_name: str, algorithm: str, permutation: str, api_url: str
+) -> List[Hash]:
+    request_url = urljoin(
+        api_url, f"/module/{module_name:s}/{algorithm:s}/{permutation:s}"
+    )
+    logger.log_debug(f"get_module_hashes requested URL: {request_url}")
+
     r = requests.get(request_url)
     if not r.ok:
         raise HashDBError(
-            f"Get hash API request failed, status {r.status_code} for URL: {request_url}"
+            f"Get hash API request failed for URL {request_url} with status code {r.status_code}"
         )
+
     results = r.json()
-    return results
+    logger.log_debug(
+        f"get_module_hashes request to URL: {request_url} returned results\n{results}"
+    )
+    try:
+        hashes = [Hash.from_dict(hash_) for hash_ in results["hashes"]]
+    except KeyError as parsing_key_error:
+        raise HashDBError(
+            f"Could not parse the following response from URL {request_url} as a valid list of hashes; parsing failed to find required key {parsing_key_error}:\n{results}"
+        )
+    return hashes
 
 
-def hunt_hash(hash_value: int, api_url: str) -> List:
+def hunt_hash(hash_value: int, api_url: str) -> List[HuntMatch]:
     matches = []
     hash_list = [hash_value]
-    request_url = api_url + "/hunt"
-    logger.log_info(f"hunt_hash requested URL: {request_url}")
-    r = requests.post(request_url, json={"hashes": hash_list})
+    request_url = urljoin(api_url, "/hunt")
+    request_data = {"hashes": hash_list}
+    logger.log_debug(
+        f"hunt_hash requested URL: {request_url} with request data\n{request_data}"
+    )
+
+    r = requests.post(request_url, json=request_data)
     if not r.ok:
-        logger.log_error(
-            f"hunt_hash request to URL {request_url} failed, using the following parameters:"
-        )
-        logger.log_error(f"hash list: {hash_list}")
-        logger.log_error(f"request body: {r.json()}")
         raise HashDBError(
-            f"Get hash API request failed, status {r.status_code} for URL: {request_url}"
+            f"Hunt hash API request failed for URL {request_url} with status code {r.status_code}, using the following sent request data:\n{request_data}"
         )
-    for hit in r.json().get("hits", []):
-        algo = hit.get("algorithm", None)
-        if (algo != None) and (algo not in matches):
-            matches.append(algo)
-    return matches
+
+    results = r.json()
+    logger.log_debug(
+        f"hunt_hash request to URL: {request_url} returned results\n{results}"
+    )
+    try:
+        matches = [HuntMatch.from_dict(hit) for hit in results["hits"]]
+    except KeyError as parsing_key_error:
+        raise HashDBError(
+            f"Could not parse the following response from URL {request_url} as a valid list of hunt matches; parsing failed to find required key {parsing_key_error}:\n{results}"
+        )
+    return sorted(matches, key=lambda match: match.count)
